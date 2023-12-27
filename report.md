2c37c43c59fdf16d1bad2dce292d20d4306b1bd6
Elisa Pioldi [eπ] <e.pioldi@campus.unimib.it>
2023-12-27 14:47:52 +0100

    Add solution 3, init report

--- report.md
+++ report.md
@@ -1,16 +1,22 @@
 ## Cryptocurrencies WS2023 - Task 6a - Payment Channels
 
-Author: `Your name here`
+Author: Elis Pioldi
 
 # Exercise 1
 TODO: Include a copy of your script here
 TODO: Explain why your solution works
 
+`OP_0` `<sigAlice>` `<sigBob>`
+
 # Exercise 2
 TODO: Include a copy of your script here
 TODO: Explain why your solution works
 
+`<sigCarol>` `<pubKeyCarol>` `<punishSecret>` `OP_1`
+
 # Exercise 3
 TODO: Include a copy of your script here
 TODO: Explain why your solution works
+
+`<sigDave>` `<pubKeyDave>` `OP_0` `OP_0` `<sigDave>` `<pubKeyDave>`
 
--- students.py
+++ students.py
@@ -20,15 +20,21 @@ def main():
     print_tx(solution_ex1, 'Transaction solution_ex1')
     post_tx(solution_ex1)  # TODO uncomment this line if you want to post the transaction to the testnet
 
+    # https://live.blockcypher.com/btc-testnet/tx/31e1aacd10835b2fca31c6bdeaa7037deeb20a370ec10927b2f199750c4e70e7/
+
     ### Exercise 2
     solution_ex2 = solve_ex2(TX_IN_EX2, CAROL, 2500)
     print_tx(solution_ex2, 'Transaction solution_ex2')
     post_tx(solution_ex2)  # TODO uncomment this line if you want to post the transaction to the testnet
 
+    # https://live.blockcypher.com/btc-testnet/tx/0956a92892681d16f3084083e3c57652bebde75e5fa572c2f48d9fa855b8fbef/
+
     ### Exercise 3
     solution_ex3 = solve_ex3(TX_IN_EX3, DAVE, 4000)
     print_tx(solution_ex3, 'Transaction solution_ex3')
-    # post_tx(solution_ex3)  # TODO uncomment this line if you want to post the transaction to the testnet
+    post_tx(solution_ex3)  # TODO uncomment this line if you want to post the transaction to the testnet
+
+    # https://live.blockcypher.com/btc-testnet/tx/b921fccb09b7a01d915ff557e0a15d1b905bfd2ff6406aaec202c204bfc157a4/
 
 def solve_ex1(tx_in: TxInput, id_alice: Id, id_bob: Id, amount_alice: int, amount_bob: int) -> Transaction:  # You can modify the parameters if you need to
     #### Create outputs
@@ -42,13 +48,10 @@ def solve_ex1(tx_in: TxInput, id_alice: Id, id_bob: Id, amount_alice: int, amoun
     sig_alice = id_alice.sk.sign_input(tx, 0, SCRIPT_PK_EX1)
     sig_bob = id_bob.sk.sign_input(tx, 0, SCRIPT_PK_EX1)
 
-    # create multisig tx
-    script = ['OP_0', sig_alice, sig_bob]
-
     #### Set unlocking script (script_sig) to match the locking script (script_pk) of the given input 
-    tx_in.script_sig = Script(script) # TODO replace 'OP_0' with your solution here
-
-    # https://live.blockcypher.com/btc-testnet/tx/31e1aacd10835b2fca31c6bdeaa7037deeb20a370ec10927b2f199750c4e70e7/
+    tx_in.script_sig = Script(['OP_0', 
+                               sig_alice, 
+                               sig_bob])
 
     print(tx)
     print(id_alice.addr)
@@ -66,16 +69,12 @@ def solve_ex2(tx_in: TxInput, id_carol: Id, amount: int) -> Transaction:  # You 
     #### Create signature(s)
     sig_carol = id_carol.sk.sign_input(tx, 0, SCRIPT_PK_EX2)
 
-    script = [
-        sig_carol,
-        id_carol.pk.to_hex(), 
-        PUNISH_SECRET,
-        'OP_1'
-    ]
-
     #### Set unlocking script (script_sig) to match the locking script (script_pk) of the given input 
-    tx_in.script_sig = Script(script) # TODO replace 'OP_0' with your solution here
-
+    tx_in.script_sig = Script([sig_carol,
+                               id_carol.pk.to_hex(), 
+                               PUNISH_SECRET,
+                               'OP_1']) 
+    
     print(tx)
     print(id_carol.addr)
 
@@ -92,7 +91,15 @@ def solve_ex3(tx_in: TxInput, id_dave: Id, amount: int) -> Transaction:  # You c
     sig_dave = id_dave.sk.sign_input(tx, 0, SCRIPT_PK_EX3)
     
     #### Set unlocking script (script_sig) to match the locking script (script_pk) of the given input 
-    tx_in.script_sig = Script(['OP_O']) # TODO replace 'OP_0' with your solution here
+    tx_in.script_sig = Script([sig_dave,
+                               id_dave.pk.to_hex(),
+                               'OP_0',
+                               'OP_0',
+                               sig_dave,
+                               id_dave.pk.to_hex()])
+
+    print(tx)
+    print(id_dave.addr)
 
     return tx
 
